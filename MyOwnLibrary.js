41b973425fe6fbf1582358eaaaffd70956651326
nahin777 <75070582+nahin777@users.noreply.github.com>
2020-12-18 15:42:01 +0530

    Add files via upload

---
+++ MyOwnLibrary.js
@@ -0,0 +1,12 @@
+function bounceOff(object1,object2){
+    if (object1.x - object2.x < object2.width/2 + object1.width/2
+      && object2.x - object1.x < object2.width/2 + object1.width/2) {
+      object1.velocityX = object1.velocityX * (-5);
+      object2.velocityX = object2.velocityX * (-5);
+    }
+    if (object1.y - object2.y < object2.height/2 + object1.height/2
+      && object2.y - object2.y < object2.height/2 + object1.height/2) {
+        object1.velocityY = object1.velocityY * (-5);
+        object2.velocityY = object2.velocityY * (-5);
+    } 
+  }
